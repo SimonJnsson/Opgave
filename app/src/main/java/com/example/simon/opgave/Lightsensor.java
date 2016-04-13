91ec161b942ea36dae1b279c7f4f9f609f063bb7
Frederik Laursen <fredefuppie@hotmail.com>
2016-04-13 12:52:10 +0200

    LightSensor commit

--- app/src/main/java/com/example/simon/opgave/Lightsensor.java
+++ app/src/main/java/com/example/simon/opgave/Lightsensor.java
@@ -1,18 +1,24 @@
 package com.example.simon.opgave;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Movie;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class Lightsensor extends AppCompatActivity {
     private SensorManager mSensorManager;
     private Sensor mLight;
     private TextView tv;
+    private Movie movie;
 
 
     @Override
@@ -24,13 +30,19 @@ public class Lightsensor extends AppCompatActivity {
         mLight = mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
         mSensorManager.registerListener(LightSensorListener, mLight, mSensorManager.SENSOR_DELAY_NORMAL);
         tv = (TextView) findViewById(R.id.textView_Light);
+
     }
 
     private final SensorEventListener LightSensorListener = new SensorEventListener() {
         @Override
         public void onSensorChanged(SensorEvent event) {
-            if (event.sensor.getType() == Sensor.TYPE_LIGHT) {
+            if (event.sensor.getType() == Sensor.TYPE_LIGHT)
+            {
                 tv.setText("Light: " + event.values[0]);
+            }
+            else
+            {
+                //No sensor
             }
         }
 
